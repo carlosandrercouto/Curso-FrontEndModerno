a9ddac62a49f5db97f7ed5a9c2d419b3f2fd5dee
Carlos Andre C <carlosandrecouto@live.com>
2017-10-19 13:55:54 -0200

    Atualizacao repo

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+	gulp.src('./src/sass/**/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('./src/css/'));
+})
+
+gulp.task('listen', function(){
+	gulp.watch('./src/sass/**/*.scss', ['sass']);
+
+})
